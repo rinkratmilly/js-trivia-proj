01b967da43b8f25ad96d519b3fa4e657611beb38
rinkratmilly <patrick.milligan4@gmail.com>
2021-07-29 15:53:48 -0400

    first few items off list

---
+++ script.js
@@ -0,0 +1,17 @@
+// event listeners for buttons, (forEach)
+// innerHTML dom manip. for question and answer propogation
+// function that iterates correct class to randomize answers
+// external sheet to relate answers to questions
+// multiple html,css,js files to correspond to different page states 
+
+
+
+
+const gameBoard = document.querySelector('.game-board');
+const gameHeader = document.querySelector('.game-header');
+const gameFooter = document.querySelector('.game-footer');
+
+
+let contents = document.querySelector(h1)
+
+console.log('works')
