20b98f9c73f7adbf2f643540b8bf7d1fd851c73d
rinkratmilly <patrick.milligan4@gmail.com>
2021-07-29 22:53:59 -0400

    early psuedocode and testing

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+// --psuedocode--
+
 // event listeners for buttons, (forEach)
 // innerHTML dom manip. for question and answer propogation
 // function that iterates correct class to randomize answers
 // external sheet to relate answers to questions
 // multiple html,css,js files to correspond to different page states 
 
+// --psuedocode--
 
 
-
+// some syntax examples pulled from lab for reference
 const gameBoard = document.querySelector('.game-board');
 const gameHeader = document.querySelector('.game-header');
 const gameFooter = document.querySelector('.game-footer');
 
 
-let contents = document.querySelector(h1)
+// testing different dom methods to access elements
+ document.querySelector('h1').innerHTML = 'code test'
+document.querySelector('#ex-code').innerText = 'next code'
+let test = document.getElementsByClassName('button-field').addEventListener('click', function(e) {
+    if (e.target && e.target.nodeName == 'button'){
+    console.log('clicked')
 
-console.log('works')
+}
+test();
+
+
+// console.log('works')
