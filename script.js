8fd77452e9daadbccc30a20d10c1c8c145dde8d5
rinkratmilly <patrick.milligan4@gmail.com>
2021-08-02 09:16:39 -0400

    updated notes issues w/ hw submission

--- script.js
+++ script.js
@@ -7,11 +7,12 @@
 // function that iterates correct class to randomize answers
 // external sheet to relate answers to questions
 // multiple html,css,js files to correspond to different page states 
+// sort w/ ternary to compare if answers are correct
 
 // --psuedocode--
 
 
-// some syntax examples pulled from lab for reference
+some syntax examples pulled from lab for reference
 const gameBoard = document.querySelector('.game-board');
 const gameHeader = document.querySelector('.game-header');
 const gameFooter = document.querySelector('.game-footer');
@@ -19,7 +20,7 @@ const gameFooter = document.querySelector('.game-footer');
 
 // testing different dom methods to access elements
  document.querySelector('h1').innerHTML = 'code test'
-document.querySelector('#ex-code').innerText = 'next code'
+document.getElementById('#ex-code').innerText = 'next code'
 let test = document.getElementsByClassName('button-field').addEventListener('click', function(e) {
     if (e.target && e.target.nodeName == 'button'){
     console.log('clicked')
@@ -28,4 +29,5 @@ let test = document.getElementsByClassName('button-field').addEventListener('cli
 test();
 
 
-// console.log('works')
+
+console.log('works')
